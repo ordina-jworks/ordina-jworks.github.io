b5df3f85589b1d4ed9324e31a504dbb7998cc138
Tim Ysewyn <Tim.Ysewyn@ordina.be>
2016-03-06 18:36:10 +0100

    Updated author page generator

--- _plugins/author_page_generator.rb
+++ _plugins/author_page_generator.rb
@@ -1,6 +1,6 @@
 #------------------------------------------------------------------------
 # encoding: utf-8
-# @(#)author_generator.rb	1.00 05-Mar-2016 20:37
+# @(#)author_page_generator.rb	1.00 05-Mar-2016 20:37
 #
 # Copyright (c) 2016 Tim Ysewyn for JWorks. All Rights Reserved.
 # Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
@@ -12,20 +12,17 @@
 # Included filters : (none)
 #------------------------------------------------------------------------
 
-require 'yaml'
-
 module Jekyll
     class AuthorPage < Page
-        def initialize( site, base, author, data )
+        def initialize( site, base, author )
             @site = site
             @base = base
-            @dir = data["permalink"]
+            @dir = author["permalink"]
             @name = 'index.html'
             
             self.process(@name)
             self.read_yaml(File.join(base, '_layouts'), 'author.html')
-            self.data['author'] = data
-            self.data['bio'] = "#{author}.md"
+            self.data['author'] = author
         end
     end
     
@@ -34,8 +31,8 @@ module Jekyll
 
         def generate( site )
             if site.layouts.key? 'author'
-                site.data["authors"].each do |author, data|
-                    site.pages << AuthorPage.new( site, site.source, author, data )
+                site.data["authors"].each_value do |author|
+                    site.pages << AuthorPage.new( site, site.source, author )
                 end
             end
         end
