0585c960ca8891e7c72eeb0c6b5f1cdbe4247037
Gert-jan Theunissen <gertjan.theunissen@nike.com>
2018-02-19 08:57:36 +0100

    Use destination from global configuration (#144)

--- _plugins/hooks/site/post_read/blur_image.rb
+++ _plugins/hooks/site/post_read/blur_image.rb
@@ -4,9 +4,11 @@ require 'blur_image'
 Jekyll::Hooks.register :site, :post_write do |site|
     puts 'Blur posts background images'
 
+    @base = site.config['destination']
+
     site.posts.docs.each { |post|
-        @image = '_site' + post.data['image']
-        original_image = File.read(@image)
+        @image = post.data['image']
+        original_image = File.read(File.join(@base, @image))
         blurred_image = BlurImage.blur(original_image, 5)
 
         @image.insert(@image.rindex('.'), '_blur')
