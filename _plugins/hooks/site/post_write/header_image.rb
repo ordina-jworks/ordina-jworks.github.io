dddcf9c488d9b659b21c1bcc7f83301931c52054
Gert-jan Theunissen <gert-jan.theunissen@ordina.be>
2018-10-08 19:09:39 +0200

    Skip image blur when file already exists + keep files in img directory when rebuilding (#387)

--- _plugins/hooks/site/post_write/header_image.rb
+++ _plugins/hooks/site/post_write/header_image.rb
@@ -3,12 +3,17 @@ Jekyll::Hooks.register :site, :post_write do |site|
 
     @base = site.config['destination']
 
-    site.posts.docs.each { |post|
+    site.posts.docs.each {|post|
         @image = File.join(@base, post.data['image'])
-        original_image = File.read(@image)
-        blurred_image = HeaderImageCreator.create(original_image)
+        @blurred_image = @image.clone
+        @blurred_image.insert(@blurred_image.rindex('.'), '_header')
 
-        @image.insert(@image.rindex('.'), '_header')
-        blurred_image.write(@image)
+        if !File.file?(@blurred_image)
+            original_image = File.read(@image)
+            blurred_image = HeaderImageCreator.create(original_image)
+
+            @image.insert(@image.rindex('.'), '_header')
+            blurred_image.write(@image)
+        end
     }
 end
